54bf17558fb75324cfa331d75faac0cd724fa0bc
Indri Himawan <indri.himawan18@yahoo.com>
2015-09-30 00:51:00 -0500

    Added database THAT DOES NOT WORK

--- app/src/main/java/com/example/kakoi/MainPlay.java
+++ app/src/main/java/com/example/kakoi/MainPlay.java
@@ -8,17 +8,37 @@ import android.widget.TextView;
 
 public class MainPlay extends AppCompatActivity {
 
+    EditText myInput;
+    TextView myText;
+    MyDBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_play);
-
+        myInput = (EditText) findViewById(R.id.myInput);
+        myText = (TextView) findViewById(R.id.myText);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+        printDatabase();
     }
 
+    public void addButtonClicked(View view){
+        Products product = new Products(myInput.getText().toString());
+        dbHandler.addProduct(product);
+        printDatabase();
+    }
 
+    public void deleteButtonClicked(View view){
+        String inputText = myInput.getText().toString();
+        dbHandler.deleteProduct(inputText);
+        printDatabase();
+    }
 
-
+    public void printDatabase(){
+        String dbString = dbHandler.databaseToString();
+        myText.setText(dbString);
+        myInput.setText("");
+    }
 
 
 }
